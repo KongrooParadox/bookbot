eb7b61429766bd33265c726a7154aa3d49e11525
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2024-09-29 15:56:52 +0200

    feat: count characters in book

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def main() -> str :
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    print(get_number_of_words(book_text))
-
+    print(f"Book {book_path} has {get_number_of_words(book_text)} words.")
+    print(f"Character count: {get_character_count(book_text)}")
 
 def get_book_text(path) -> str:
     with open(path) as f:
@@ -10,5 +10,15 @@ def get_book_text(path) -> str:
 
 def get_number_of_words(text) -> int:
     return len(text.split())
+
+def get_character_count(text) -> dict:
+    char_count = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 main()
